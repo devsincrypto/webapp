97226cc59148b36ab7af0594694fe96d909d6b0d
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-05-15 20:02:27 +0200

    Add simpleanalytics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,19 @@ export function Footer(): React.ReactElement {
 			<div className="thin-container section text-center">
 				<p>© Devs in Crypto 2021</p>
 
+				<p>
+					Made by a crypto dev based in Paris, 🇫🇷.
+					<br />
+					<a href="mailto:amaury@devsincrypto.com">Support Email</a>
+					{' - '}
+					<a
+						href="https://github.com/devsincrypto/webapp"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Github
+					</a>
+				</p>
 				<a
 					href="https://vercel.com?utm_source=devsincrypto&utm_campaign=oss"
 					target="_blank"
